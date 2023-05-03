254fea1d9519e9ac9940c6d4f08010dc11c23b14
dhananjay <dhananjaykumarverma088@gmail.com>
2023-05-03 17:41:01 +0530

    new

--- App.js
+++ App.js
@@ -1,20 +1,129 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet, Image, FlatList } from 'react-native';
+import ImagePicker from 'react-native-image-picker';
+import Video from 'react-native-video';
 
-export default function App() {
+const PostFeed = () => {
+  const [text, setText] = useState('');
+  const [image, setImage] = useState(null);
+  const [video, setVideo] = useState(null);
+  const [posts, setPosts] = useState([]);
+
+  const handleTextChange = (text) => {
+    setText(text);
+  };
+
+  const handleImageSelect = () => {
+    ImagePicker.showImagePicker({}, (response) => {
+      if (response.uri) {
+        setImage(response.uri);
+      }
+    });
+  };
+
+  const handleVideoSelect = () => {
+    ImagePicker.showImagePicker({ mediaType: 'video' }, (response) => {
+      if (response.uri) {
+        setVideo(response.uri);
+      }
+    });
+  };
+  
+
+
+
+
+const handlePostSubmit = () => {
+    const newPost = { text, image, video };
+    setPosts([...posts, newPost]);
+    setText('');
+    setImage(null);
+    setVideo(null);
+  };
+
+  const renderItem = ({ item }) => {
+    return (
+      <View style={styles.postContainer}>
+        {item.text !== '' && (
+          <Text style={styles.postText}>{item.text}</Text>
+        )}
+        {item.image !== null && (
+          <Image style={styles.postImage} source={{ uri: item.image }} />
+        )}
+        {item.video !== null && (
+          <Video style={styles.postVideo} source={{ uri: item.video }} />
+        )}
+      </View>
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.formContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Write your post here"
+          value={text}
+          onChangeText={handleTextChange}
+        />
+        <View style={styles.buttonContainer}>
+          <Button title="Select Image" onPress={handleImageSelect} />
+          <Button title="Select Video" onPress={handleVideoSelect} />
+          <Button title="Post" onPress={handlePostSubmit} />
+        </View>
+        {image !== null && (
+          <Image style={styles.previewImage} source={{ uri: image }} />
+        )}
+        {video !== null && (
+          <Video
+            style={styles.previewVideo}
+            source={{ uri: video }}
+            resizeMode="contain"
+            repeat={true}
+          />
+        )}
+      </View>
+      <FlatList
+        data={posts}
+        renderItem={renderItem}
+        keyExtractor={(item, index) => index.toString()}
+      />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 10,
   },
+  formContainer: {
+    marginBottom: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 10,
+    marginBottom: 10,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginBottom: 10,
+  },
+  previewImage: {
+    width: '100%',
+    height: 200,
+    resizeMode: 'contain',
+    marginBottom: 10,
+  },
+  previewVideo: {
+    width: '100%',
+    height: 200,
+    marginBottom: 10,
+  },
+  postContainer: {
+    marginBottom: 20,
+},
 });
+export default PostFeed;
---
+++ readme.md
@@ -0,0 +1,19 @@
+I appreciate your interest in our job opening. Share your email address for further communication (ignore if you have already mentioned it as part of your application).
+
+As a next step, we are expecting you to complete a short assignment by Wednesday 5 PM.
+
+
+ The assignment is to develop a basic screen in react native which allows posting a post (can include text, image, and video) and seeing posts from others.
+ 
+ 
+  You can share the project via GitHub repo or upload it to Google Drive for evaluation. 
+
+Thanks,
+Kuldeep
+Submission deadline: 03 May, 2023
+11:42 PM
+02 May
+Sure, I will submit
+12:42 AM
+dhananjayverma9546@gmail.com
+12:42 AM
